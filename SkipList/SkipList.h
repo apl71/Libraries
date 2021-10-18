73ce6950cbac33663b66c0cc69e255a3b3746f10
apl <luyingjie1998@outlook.com>
2021-10-18 15:30:37 +0800

    debug

--- SkipList/SkipList.h
+++ SkipList/SkipList.h
@@ -3,6 +3,7 @@
 
 #include <cstdint>
 #include <random>
+#include <iostream>
 
 enum ERR_CODE { SUCCESS = 1, NO_SUCH_NODE = 0, KEY_ALREADY_EXISTS = -1 };
 
@@ -22,21 +23,32 @@ private:
     } *head_node;
     uint16_t max_level;
     uint16_t highest_level;
+    KEY max_key;
+    double p = 0.5;
 
-    int16_t Find(const KEY &key, Node *&node, uint16_t *pass) const;
+    ERR_CODE Find(const KEY &key, Node *&node, Node **pass) const;
 
     uint16_t RandomLevel(const double p) const;
 
 public:
     SkipList(const uint16_t max, const KEY &max_key_value);
 
-    int16_t Search(const KEY &key, VALUE &value) const;
+    ~SkipList();
 
-    int16_t Insert(const KEY &key, const VALUE &value);
+    ERR_CODE Search(const KEY &key, VALUE &value) const;
+
+    ERR_CODE Insert(const KEY &key, const VALUE &value);
+
+    ERR_CODE Remove(const KEY &key);
+
+    // For debug
+    void PrintAll();
+
+    void Clear();
 };
 
 template<typename KEY, typename VALUE>
-int16_t SkipList<KEY, VALUE>::Find(const KEY &key, Node *&node, uint16_t *pass) const
+ERR_CODE SkipList<KEY, VALUE>::Find(const KEY &key, Node *&node, Node **pass) const
 {
     node = head_node;
     for (int32_t i = highest_level; i >= 0; --i)
@@ -50,9 +62,9 @@ int16_t SkipList<KEY, VALUE>::Find(const KEY &key, Node *&node, uint16_t *pass) 
             pass[i] = node;
         }
     }
-    if (node->forward[i] && node->forward[i]->key == key) // 找到了key
+    if (node->forward[0] && node->forward[0]->key == key) // 找到了key
     {
-        node = node->forward[i];
+        node = node->forward[0];
         return SUCCESS;
     }
     else // 没有找到
@@ -64,8 +76,8 @@ int16_t SkipList<KEY, VALUE>::Find(const KEY &key, Node *&node, uint16_t *pass) 
 template<typename KEY, typename VALUE>
 uint16_t SkipList<KEY, VALUE>::RandomLevel(const double p) const
 {
-    std::default_random_engine e;
-    std::uniform_real_distribution<double> u(0, 1);
+    static std::default_random_engine e;
+    static std::uniform_real_distribution<double> u(0, 1);
     uint16_t level = 0;
     while (u(e) < p && level <= max_level)
     {
@@ -80,6 +92,7 @@ SkipList<KEY, VALUE>::SkipList(const uint16_t max, const KEY &max_key_value)
     // 初始化相关信息
     max_level = max;
     highest_level = 0;
+    max_key = max_key_value;
     // 构建根节点
     head_node = new Node;
     head_node->level = highest_level;
@@ -90,12 +103,18 @@ SkipList<KEY, VALUE>::SkipList(const uint16_t max, const KEY &max_key_value)
     }
     // 构建最后的节点
     Node *last_node = head_node->forward[0] = new Node;
-    last_node->key = max_key_value;
+    last_node->key = max_key;
     last_node->level = highest_level;
 }
 
 template<typename KEY, typename VALUE>
-int16_t SkipList<KEY, VALUE>::Search(const KEY &key, VALUE &value) const
+SkipList<KEY, VALUE>::~SkipList()
+{
+    Clear();
+}
+
+template<typename KEY, typename VALUE>
+ERR_CODE SkipList<KEY, VALUE>::Search(const KEY &key, VALUE &value) const
 {
     Node *node = nullptr;
     ERR_CODE code = Find(key, node, nullptr);
@@ -111,16 +130,116 @@ int16_t SkipList<KEY, VALUE>::Search(const KEY &key, VALUE &value) const
 }
 
 template<typename KEY, typename VALUE>
-int16_t SkipList<KEY, VALUE>::Insert(const KEY &key, const VALUE &value)
+ERR_CODE SkipList<KEY, VALUE>::Insert(const KEY &key, const VALUE &value)
 {
     Node *node = nullptr;
-    ERR_CODE code = Find(key, node);
+    Node **update = new Node *[max_level];
+    for (uint16_t i = 0; i < max_level; ++i)
+    {
+        update[i] = head_node;
+    }
+    ERR_CODE code = Find(key, node, update);
     if (code == SUCCESS)
     {
         return KEY_ALREADY_EXISTS;
     }
     // 新建节点
-    
+    Node *new_node = new Node;
+    new_node->key = key;
+    new_node->value = value;
+    new_node->level = RandomLevel(p);
+    new_node->forward = new Node *[max_level];
+
+    // 更新旧的forward列表
+    if (new_node->level > highest_level)
+    {
+        highest_level = new_node->level;
+        head_node->level = highest_level;
+    }
+    for (uint16_t i = 0; i <= highest_level; ++i)
+    {
+        new_node->forward[i] = update[i]->forward[i];
+        update[i]->forward[i] = new_node;
+    }
+
+    // 删除临时空间
+    delete[]update;
+    return SUCCESS;
+}
+
+template<typename KEY, typename VALUE>
+ERR_CODE SkipList<KEY, VALUE>::Remove(const KEY &key)
+{
+    Node *to_delete = nullptr;
+    Node **update = new Node *[highest_level + 1];
+    for (uint16_t i = 0; i < max_level; ++i)
+    {
+        update[i] = nullptr;
+    }
+    ERR_CODE code = Find(key, to_delete, update);
+    if (code == NO_SUCH_NODE)
+    {
+        return code;
+    }
+    // 更新指针
+    for (int32_t i = highest_level; i >= 0; --i)
+    {
+        update[i]->forward[i] = to_delete->forward[i];
+    }
+    // 检测是否为唯一的最高层节点
+    if (to_delete->level == highest_level)
+    {
+        // 找到下一个高度的节点
+        uint16_t new_highest = highest_level;
+        while (new_highest >= 0 &&
+               head_node->forward[new_highest] == to_delete &&
+               to_delete->forward[new_highest]->key == max_key)
+        {
+            --new_highest;
+        }
+        highest_level = new_highest;
+    }
+    // 删除节点
+    delete[](to_delete->forward);
+    delete to_delete;
+    return SUCCESS;
+}
+
+template<typename KEY, typename VALUE>
+void SkipList<KEY, VALUE>::Clear()
+{
+    Node *node = head_node;
+    while (node && node->key != max_key)
+    {
+        Node *tmp = node->forward[0];
+        delete[](node->forward);
+        delete node;
+        node = tmp;
+    }
+    delete[](node->forward);
+    delete node;
+}
+
+template<typename KEY, typename VALUE>
+void SkipList<KEY, VALUE>::PrintAll()
+{
+    Node *node = head_node;
+    while (node && node->key != max_key)
+    {
+        // std::cout << node->key << ": " << node->value << "\tlevel = " << node->level << std::endl;
+        std::cout << "节点地址：" << node << std::endl;
+        std::cout << "{" << std::endl;
+        std::cout << "\tkey     = " << node->key << std::endl;
+        std::cout << "\tvalue   = " << node->value << std::endl;
+        std::cout << "\tlevel   = " << node->level << std::endl;
+        std::cout << "\tforward = " << std::endl;
+        for (int32_t level = node->level; level >= 0; --level)
+        {
+            std::cout << "\t\tlevel: " << level << " 指向：" << node->forward[level] << std::endl;
+        }
+        std::cout << "}" << std::endl << std::endl;
+        node = node->forward[0];
+    }
 }
 
 #endif
---
+++ SkipList/test.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <climits>
+#include <random>
+#include "SkipList.h"
+
+using namespace std;
+
+string RandomString(int length)
+{
+    string str;
+    static default_random_engine e;
+    static uniform_int_distribution<int> u(33, 126);
+    for (int i = 0; i < length; ++i)
+    {
+        str.push_back(char(u(e)));
+    }
+    return str;
+}
+
+int main()
+{
+    SkipList<unsigned long long, string> skip_list(16, ULLONG_MAX);
+
+    // 顺序键插入测试
+    for (unsigned long long key = 1; key <= 50; ++key)
+    {
+        string value = RandomString(10);
+        skip_list.Insert(key, value);
+        cout << "插入元素：[" << "key = " << key << ", value = " << value << "]" << endl;
+    }
+
+    // 顺序键查找测试
+    for (unsigned long long key = 1; key <= 50; ++key)
+    {
+        string value;
+        ERR_CODE rst = skip_list.Search(key, value);
+        if (rst == SUCCESS)
+        {
+            cout << "成功查找元素：key = " << key << ", value = " << value << endl;
+        }
+        else
+        {
+            cout << "查找元素失败：key = " << key << endl;
+        }
+    }
+
+    // 顺序键删除测试
+    for (unsigned long long key = 1; key <= 0; ++key)
+    {
+        ERR_CODE rst = skip_list.Remove(key);
+        if (rst == SUCCESS)
+        {
+            cout << "成功删除元素，key = " << key << endl;
+        }
+        else
+        {
+            cout << "删除元素出错，key = " << key << endl;
+        }
+    }
+
+    skip_list.PrintAll();
+
+    return 0;
+}
